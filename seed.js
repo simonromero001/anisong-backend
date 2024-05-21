93e11f4733f05fccf7f350c13776fb5bd52d1d49
Simon Romero <simon.romero001@gmail.com>
2024-05-20 22:14:22 -0700

    Integrate MongoDB Atlas and AWS S3

--- seed.js
+++ seed.js
@@ -43,8 +43,35 @@ async function addVideo() {
       endTime: 69600,
     });
 
+    const video3 = await Video.create({
+      url: "HanaNiNatte.webm",
+      sentence: "_にして守るのがいいんじゃない",
+      word: "秘密",
+      startTime: 25517,
+      endTime: 30400,
+    });
+
+    const video4 = await Video.create({
+      url: "HanaNiNatte.webm",
+      sentence: "秘密にして_のがいいんじゃない",
+      word: "守る",
+      startTime: 25517,
+      endTime: 30400,
+    });
+
+    const video5 = await Video.create({
+      url: "HanaNiNatte.webm",
+      sentence: "その_がくだんない",
+      word: "判断",
+      startTime: 38000,
+      endTime: 40500,
+    });
+
     console.log("Video added:", video1);
     console.log("Video added:", video2);
+    console.log("Video added:", video3);
+    console.log("Video added:", video4);
+    console.log("Video added:", video5);
   } catch (error) {
     console.error("Error adding video:", error);
   }
